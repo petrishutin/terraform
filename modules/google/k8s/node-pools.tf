332fb6a23c5b8f2d923e04c0cffd1c83b972076c
petr <petr.ishutin@gmail.com>
2023-05-15 21:43:50 +0200

    Change nodes configuration

--- modules/google/k8s/node-pools.tf
+++ modules/google/k8s/node-pools.tf
@@ -3,30 +3,30 @@ resource "google_service_account" "kubernetes" {
 }
 
 
-#resource "google_container_node_pool" "general" {
-#  name       = "general"
-#  cluster    = google_container_cluster.primary.id
-#  node_count = 1
-#
-#  management {
-#    auto_repair  = true
-#    auto_upgrade = true
-#  }
-#
-#  node_config {
-#    preemptible  = false
-#    machine_type = "e2-small"
-#
-#    labels = {
-#      role = "general"
-#    }
-#
-#    service_account = google_service_account.kubernetes.email
-#    oauth_scopes = [
-#      "https://www.googleapis.com/auth/cloud-platform"
-#    ]
-#  }
-#}
+resource "google_container_node_pool" "general" {
+  name       = "general"
+  cluster    = google_container_cluster.primary.id
+  node_count = 1
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  node_config {
+    preemptible  = false
+    machine_type = "e2-small"
+
+    labels = {
+      role = "general"
+    }
+
+    service_account = google_service_account.kubernetes.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
 
 resource "google_container_node_pool" "spot_1" {
   name    = "spot-a"
@@ -111,43 +111,43 @@ resource "google_container_node_pool" "spot_2" {
   }
 }
 
-resource "google_container_node_pool" "spot_3" {
-  name    = "spot-c"
-  location   = var.location3
-  cluster = google_container_cluster.primary.id
-  node_count = 1
-
-  node_locations = [
-    var.location3
-  ]
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-
-  autoscaling {
-    min_node_count = 0
-    max_node_count = 10
-  }
-
-  node_config {
-    preemptible  = true
-    machine_type = "e2-small"
-
-    labels = {
-      type = "spot"
-      location   = var.location3
-    }
-
-    taint {
-      key    = "instance_type"
-      value  = "spot"
-      effect = "NO_SCHEDULE"
-    }
-
-    service_account = google_service_account.kubernetes.email
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
-  }
-}
+#resource "google_container_node_pool" "spot_3" {
+#  name    = "spot-c"
+#  location   = var.location3
+#  cluster = google_container_cluster.primary.id
+#  node_count = 1
+#
+#  node_locations = [
+#    var.location3
+#  ]
+#  management {
+#    auto_repair  = true
+#    auto_upgrade = true
+#  }
+#
+#  autoscaling {
+#    min_node_count = 0
+#    max_node_count = 10
+#  }
+#
+#  node_config {
+#    preemptible  = true
+#    machine_type = "e2-small"
+#
+#    labels = {
+#      type = "spot"
+#      location   = var.location3
+#    }
+#
+#    taint {
+#      key    = "instance_type"
+#      value  = "spot"
+#      effect = "NO_SCHEDULE"
+#    }
+#
+#    service_account = google_service_account.kubernetes.email
+#    oauth_scopes = [
+#      "https://www.googleapis.com/auth/cloud-platform"
+#    ]
+#  }
+#}
